00147ae5381b905ba1525b6d6ed91628c91066b8
WanderTerra <wanderleyterra@gmail.com>
2023-10-31 23:10:53 -0400

    Select Dia/Mes e Ano

--- ConexaoBD/src/main/java/org/example/Connection.java
+++ ConexaoBD/src/main/java/org/example/Connection.java
@@ -3,6 +3,7 @@ package org.example;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
@@ -17,23 +18,87 @@ public class Connection {
             conexao = DriverManager.getConnection("jdbc:mysql://bdcontador-pessoas.mysql.database.azure.com:3306/contadorpessoas", "wanderley", "Java&claudio");
             // Consulta SQL para encontrar o dia com a maior contagem
             ResultSet rs = conexao.createStatement().executeQuery(
-                    "SELECT DAY(data_hora) AS DiaComMaiorContagem, COUNT(*) AS Contagem " +
+                    "SELECT DATE_FORMAT(data_hora, '%d/%m/%y') AS DiaComMaiorContagem, COUNT(*) AS Contagem " +
                             "FROM registropython " +
-                            "GROUP BY DAY(data_hora) " +
+                            "GROUP BY DiaComMaiorContagem " +
                             "ORDER BY Contagem DESC " +
                             "LIMIT 1"
             );
 
             if (rs.next()) {
-                int dayWithMaxCount = rs.getInt("DiaComMaiorContagem");
+                String dayWithMaxCount = rs.getString("DiaComMaiorContagem");
                 int maxCount = rs.getInt("Contagem");
 
-                System.out.println("Dia com a maior contagem: Dia " + dayWithMaxCount + " com uma contagem de " + maxCount);
+                System.out.println("Dia com a maior contagem: " + dayWithMaxCount + " com uma contagem de " + maxCount);
             } else {
                 System.out.println("Nenhum dado encontrado.");
             }
         } catch (SQLException | ClassNotFoundException ex) {
-            System.out.println("Driver do banco de dados não encontrado.");
+            System.err.println("Erro SQL: " + ex.getMessage());
+        } finally {
+            try {
+                if (conexao != null) {
+                    conexao.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conexao = DriverManager.getConnection("jdbc:mysql://bdcontador-pessoas.mysql.database.azure.com:3306/contadorpessoas", "wanderley", "Java&claudio");
+            // Consulta SQL para encontrar o dia com a maior contagem
+            ResultSet rs = conexao.createStatement().executeQuery(
+                    "SELECT DATE_FORMAT(data_hora, '%m/%Y') AS MesComMaiorContagem, COUNT(*) AS Contagem " +
+                            "FROM registropython " +
+                            "GROUP BY MesComMaiorContagem " +
+                            "ORDER BY Contagem DESC " +
+                            "LIMIT 1"
+            );
+
+            if (rs.next()) {
+                String MonthWithMaxCount = rs.getString("MesComMaiorContagem");
+                int maxCount = rs.getInt("Contagem");
+
+                System.out.println("Mês com a maior contagem: " + MonthWithMaxCount + " com uma contagem de " + maxCount);
+            } else {
+                System.out.println("Nenhum dado encontrado.");
+            }
+        } catch (SQLException | ClassNotFoundException ex) {
+            System.err.println("Erro SQL: " + ex.getMessage());
+        } finally {
+            try {
+                if (conexao != null) {
+                    conexao.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conexao = DriverManager.getConnection("jdbc:mysql://bdcontador-pessoas.mysql.database.azure.com:3306/contadorpessoas", "wanderley", "Java&claudio");
+            // Consulta SQL para encontrar o dia com a maior contagem
+            ResultSet rs = conexao.createStatement().executeQuery(
+                    "SELECT DATE_FORMAT(data_hora, '%Y') AS AnoComMaiorContagem, COUNT(*) AS Contagem " +
+                            "FROM registropython " +
+                            "GROUP BY AnoComMaiorContagem " +
+                            "ORDER BY Contagem DESC " +
+                            "LIMIT 1"
+            );
+
+            if (rs.next()) {
+                String YearWithMaxCount = rs.getString("AnoComMaiorContagem");
+                int maxCount = rs.getInt("Contagem");
+
+                System.out.println("Ano com a maior contagem: " + YearWithMaxCount + " com uma contagem de " + maxCount);
+            } else {
+                System.out.println("Nenhum dado encontrado.");
+            }
+        } catch (SQLException | ClassNotFoundException ex) {
+            System.err.println("Erro SQL: " + ex.getMessage());
         } finally {
             try {
                 if (conexao != null) {
